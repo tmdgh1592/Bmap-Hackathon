f3e95d1fc34f23efcc406bc97dc15d53fed23b9a
tmdgh1592 <tmdgh1592@naver.com>
2022-08-23 09:56:34 +0900

    feat: 메인 액티비티 생성

---
+++ presentation/src/main/java/com/app/hackathon/presentation/view/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.app.hackathon.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.hackathon.presentation.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
